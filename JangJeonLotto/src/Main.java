a15e6912ded02e1dde97ab0b721aa4dc96032e7d
Administrator <Administrator@SC-202106161522>
2022-06-29 11:38:55 +0900

    팀원 2 재입장합니댜아

--- JangJeonLotto/src/Main.java
+++ JangJeonLotto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("영빈아 사랑해 나랑 결혼하댜");
+		System.out.println("우리 결혼은 5월에 하댜");
 		System.out.println("소스 코드 변경!");
 		System.out.println("소스코드 내가 변경한닷 - 인하");
 		System.out.println("왜 안됐지????????");
