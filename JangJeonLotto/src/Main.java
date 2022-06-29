f548c6088b2e5dafed348d11f5cb9c6d49a1da63
Administrator <Administrator@SC-202106161522>
2022-06-29 11:15:41 +0900

    변경변경 팀원1

--- JangJeonLotto/src/Main.java
+++ JangJeonLotto/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("코인 장전 로또! :3!");
 		System.out.println("소스 코드 변경!");
+		System.out.println("소스코드 내가 변경한닷 - 인하");
 	}
 }
