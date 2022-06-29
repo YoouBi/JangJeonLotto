d5f3f235f26d1926bfd3feb62a27114d87e65976
Administrator <Administrator@SC-202106161522>
2022-06-29 11:22:25 +0900

    팀원2 입장합니댜 뿌뿌

--- JangJeonLotto/src/Main.java
+++ JangJeonLotto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("코인 장전 로또! :3!");
+		System.out.println("영빈아 사랑해 나랑 결혼하댜");
 		System.out.println("소스 코드 변경!");
 		System.out.println("소스코드 내가 변경한닷 - 인하");
 		System.out.println("왜 안됐지????????");
